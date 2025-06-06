02ca5fcdc60945df243222f0db9bf44ade32db8a
lqk <1501036045@qq.com>
2025-06-06 08:21:56 +0800

    123木头人

--- src/views/BMS/RandomNotes/RandomNotes.vue
+++ src/views/BMS/RandomNotes/RandomNotes.vue
@@ -11,7 +11,20 @@
         <header-detail :title="title"></header-detail>
         <div class="list-notes-box">
           <div class="published-box">
-            <div v-for="(item, idx) in 8" :key="idx" class="published-content"></div>
+            <div v-for="(item, idx) in 8" :key="idx" class="published-content">
+              <div class="title-box">
+                <div class="title-box-left">
+                  <h3>端午节{{ item }}</h3>
+                  <img src="@/assets/images/Weather/snow.svg" />
+                </div>
+                <n-button class="delete-btn">
+                  <img src="@/assets/images/Delete.svg" />
+                </n-button>
+              </div>
+              <span>2025/05/31 12:02:32</span>
+              <p>今天端午节，下午去看龙舟比赛，希望不要下雨哈哈哈</p>
+              <div class="content-img-box"><img src="@/assets/wallpaper/nanning.jpg" /></div>
+            </div>
           </div>
           <div class="edit-release-box"></div>
         </div>
@@ -25,7 +38,6 @@ import HeaderView from '@/views/BMS/components/HeaderView.vue'
 import NavigaMenu from '@/views/BMS/components/NavigaMenu.vue'
 import HeaderDetail from '@/views/BMS/components/HeaderDetail.vue'
 const title = '随笔随记'
-
 </script>
 
 <style lang="scss" scoped>
@@ -39,19 +51,51 @@ const title = '随笔随记'
     max-height: 84vh;
     height: 90vh;
     .published-box {
-      width: 320px;
+      flex: 1;
+      min-width: 320px;
       max-height: 100%;
       overflow-y: auto;
-      border: 1px solid #000;
       .published-content {
-        height: 240px;
+        height: 180px;
+        min-height: 180px;
+        background-color: #2e33380d;
+        margin-bottom: 20px;
+        padding: 20px;
+        display: flex;
+        border-radius: 10px;
+        flex-direction: column;
+        .title-box {
+          display: flex;
+          justify-content: space-between;
+          .title-box-left {
+            img {
+              width: 12px;
+              height: 12px;
+            }
+          }
+          .delete-btn {
+            img {
+              width: 12px;
+              height: 12px;
+            }
+          }
+        }
+        .content-img-box {
+          img {
+            width: 84px;
+            height: 84px;
+          }
+        }
+      }
+      .published-content:last-child {
+        margin-bottom: 0;
       }
     }
     .edit-release-box {
-      flex: 1;
+      width: 680px;
+      min-width: 440px;
       border: 1px solid #000;
     }
   }
-
 }
 </style>
