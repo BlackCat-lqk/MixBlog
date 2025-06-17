9076c4bc92e6b54ca065ea646aee3b06f0bdc31b
lqk <1501036045@qq.com>
2025-06-17 08:15:59 +0800

    文章分类组管理更新

--- src/http/categoryTags.ts
+++ src/http/categoryTags.ts
@@ -8,3 +8,12 @@ import request from '@/utils/request'
 export function upsertCategoryTags(params: object) {
   return request.post('/upsertCategoryTags', params)
 }
+
+/**
+ * 查询分类标签
+ * @param {Object} params - 查询参数
+ * @returns {Promise}
+ */
+export function getCategoryTagsApi(params: string) {
+  return request.get(`/getCategoryTags?type=${params}`)
+}
--- src/views/BMS/components/ClassifyManage.vue
+++ src/views/BMS/components/ClassifyManage.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="classify-manage-box">
     <div class="classify-box">
-      <n-tag size="large" round v-for="(item, idx) in 27" :key="idx">
-        分类{{ item }}：
-        <span>99</span>
+      <n-tag size="large" round v-for="(item, idx) in state.categoryArray" :key="idx">
+        {{ item }}：
+        <span>0</span>
       </n-tag>
       <div v-if="isAddInput">
-        <n-input v-model:value="state.value" :style="{ width: '240px' }"> </n-input>
-        <n-button type="primary" ghost @click="isAddInput = !isAddInput"> 取消 </n-button>
+        <n-input v-model:value="state.categoryvalue" :style="{ width: '240px' }"> </n-input>
+        <n-button type="primary" ghost @click="closeAddCategory"> 取消 </n-button>
         <n-button type="info" @click="confirmAddCategory"> 确认 </n-button>
       </div>
-
       <n-button v-else secondary round strong @click="isAddInput = !isAddInput">
         <img src=" @/assets/images/Add.svg" />
         Add
@@ -22,26 +21,33 @@
         管理分组
       </n-button>
     </div>
-    <div>
-      <n-modal
-        v-model:show="showModal"
-        preset="dialog"
-        title="确认"
-        content="你确认"
-        positive-text="确认"
-        negative-text="算了"
-        @positive-click="showModal = false"
-        @negative-click="showModal = false"
-      />
-    </div>
+    <n-modal
+      style="width: auto"
+      v-model:show="showModal"
+      preset="dialog"
+      title="分类管理"
+      negative-text="关闭"
+      @positive-click="showModal = false"
+      @negative-click="showModal = false"
+    >
+      <div>
+        <n-data-table
+          :columns="tableColumns"
+          :data="state.categoryData"
+          :row-key="rowKey"
+          @update:checked-row-keys="handleTableCheck"
+        />
+      </div>
+    </n-modal>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useUserInfoStore } from '@/stores/userInfo'
-import { ref, reactive } from 'vue'
-import { upsertCategoryTags } from '@/http/categoryTags'
-import { useMessage } from 'naive-ui'
+import { ref, reactive, onMounted, h } from 'vue'
+import { upsertCategoryTags, getCategoryTagsApi } from '@/http/categoryTags'
+import type { DataTableColumns, DataTableRowKey } from 'naive-ui'
+import { NButton, useMessage } from 'naive-ui'
 
 const isAddInput = ref(false)
 const showModal = ref(false)
@@ -49,9 +55,10 @@ const userInfoStore = useUserInfoStore()
 
 const message = useMessage()
 const state = reactive({
-  value: '',
+  categoryvalue: '',
   categoryArray: [],
-  tagsArray: []
+  tagsArray: [],
+  categoryData: [],
 })
 interface CategoryTagsType {
   uid: string
@@ -65,22 +72,148 @@ const params: CategoryTagsType = reactive({
   email: '',
   type: 'article',
   category: [],
-  tags: []
+  tags: [],
 })
+const checkedRowKeysRef = ref<DataTableRowKey[]>([])
+interface category {
+  _id: string
+  uid: string
+  email: string
+  type: string
+  category: string[]
+  tags: string[]
+  createdAt: string
+  updatedAt: string
+}
+const rowKey = (row: category) => {
+  return row._id
+}
+// 获取选中的行
+const handleTableCheck = async (rowKeys: DataTableRowKey[]) => {
+  checkedRowKeysRef.value = rowKeys
+  console.log(checkedRowKeysRef.value)
+}
+function createColumns({
+  operateEdit,
+  operateDelete,
+}: {
+  operateEdit: (row: category) => void
+  operateDelete: (row: category) => void
+}): DataTableColumns<category> {
+  return [
+    {
+      type: 'selection',
+    },
+    {
+      title: '操作人',
+      key: 'email',
+    },
+    {
+      title: '类型',
+      key: 'type',
+    },
+    {
+      title: '分类',
+      key: 'category',
+    },
+    {
+      title: '创建时间',
+      key: 'createdAt',
+    },
+    {
+      title: '更新时间',
+      key: 'updatedAt',
+    },
+    {
+      title: 'Operate',
+      key: 'operate',
+      render(row) {
+        return [
+          h(
+            NButton,
+            {
+              strong: true,
+              type: 'info',
+              size: 'small',
+              onClick: () => {
+                operateEdit(row)
+              },
+            },
+            { default: () => 'Edit' },
+          ),
+          h(
+            NButton,
+            {
+              strong: true,
+              type: 'error',
+              size: 'small',
+              style: { marginLeft: '5px' },
+              onClick: () => {
+                operateDelete(row)
+              },
+            },
+            { default: () => 'Delete' },
+          ),
+        ]
+      },
+    },
+  ]
+}
+const tableColumns = createColumns({
+  operateEdit: (row) => {
+    message.info(`Delete ${row}`)
+  },
+  operateDelete: (row) => {
+    message.info(`Delete ${row}`)
+  },
+})
+// 获取分类数据
+const getCategory = async () => {
+  const params = 'article'
+  const response = await getCategoryTagsApi(params)
+  const res = response.data
+  if (res.code == 200) {
+    if (res.data.category.length > 0) {
+      state.categoryArray = res.data.category
+      state.categoryData = res.data.category.map((item: string) => {
+        return {
+          email: res.data.email,
+          type: res.data.type,
+          category: item,
+          createdAt: res.data.createdAt,
+          updatedAt: res.data.updatedAt,
+        }
+      })
+    }
+  } else {
+    message.error(res.message)
+  }
+}
+// 关闭添加分类input
+const closeAddCategory = () => {
+  isAddInput.value = false
+  state.categoryvalue = ''
+}
+// 确认添加分类
 const confirmAddCategory = async () => {
   params.uid = userInfoStore.data.user._id
   params.email = userInfoStore.data.user.email
-  params.category = [state.value]
+  params.category = [...state.categoryData, state.categoryvalue]
   params.tags = state.tagsArray
   const response = await upsertCategoryTags(params)
   const res = response.data
   if (res.code === 200) {
+    closeAddCategory()
+    getCategory()
     message.success(res.message)
-  }else {
+  } else {
     message.error(res.message)
   }
-
 }
+
+onMounted(() => {
+  getCategory()
+})
 </script>
 
 <style scoped lang="scss">
