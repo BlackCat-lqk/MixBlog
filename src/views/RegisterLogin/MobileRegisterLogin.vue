6654155ee4cf65510140ed7c6005962701a8d094
lqk <1501036045@qq.com>
2025-06-30 18:00:47 +0800

    更新h5

--- src/views/RegisterLogin/MobileRegisterLogin.vue
+++ src/views/RegisterLogin/MobileRegisterLogin.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <div>
-      <span :class="isLogin ? 'active' : 'defalt'" @click="isLogin = true">Login</span>|<span
-              :class="!isLogin ? 'active' : 'defalt'"
-              @click="isLogin = false"
-              >Register</span
-            >
+  <div class="register-login-main-box">
+      <div class="register-login-box">
+        <div class="left-info-box">
+          <div class="content-box">
+            <div class="title-box">
+              <span :class="isLogin ? 'active' : 'defalt'" @click="isLogin = true">Login</span
+              >&nbsp;|&nbsp;<span :class="!isLogin ? 'active' : 'defalt'" @click="isLogin = false"
+                >Register</span
+              >
+            </div>
+            <div class="form-box">
+              <login-view v-if="isLogin"></login-view>
+              <register-view v-if="!isLogin"></register-view>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <login-view v-if="isLogin"></login-view>
-    <register-view v-if="!isLogin"></register-view>
-  </div>
 </template>
 
 <script lang="ts" setup>
@@ -22,17 +29,76 @@ const isLogin = ref(true)
 </script>
 
 <style scoped lang="scss">
-.defalt {
-        color: rgb(199, 199, 199);
-        font-size: 16px;
-        margin-left: 10px;
-        margin-right: 10px;
+@keyframes smoothGradientFlow {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+.register-login-main-box {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 200% 200%;
+  backdrop-filter: blur(10px);
+  animation: smoothGradientFlow 15s ease infinite;
+  @include g.flexCenter;
+  .register-login-box {
+    display: flex;
+    width: 200px;
+    min-width: 200px;
+    max-width: 300px;
+    min-height: 400px;
+    height: auto;
+    @include g.borderRadius(10px);
+    box-shadow: 0 0 5px 2px rgba(75, 75, 75, 0.1);
+    -webkit-backdrop-filter: blur(30px);
+    backdrop-filter: blur(30px);
+    background-color: rgba(255, 255, 255, 0.2);
+    .left-info-box {
+      @include g.borderRadius(10px);
+      flex: 1;
+      padding: 100px 20px 20px 50px;
+      .content-box {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        .title-box {
+          margin-bottom: 30px;
+          span {
+            display: inline-block;
+            &:hover {
+              cursor: pointer;
+            }
+          }
+        }
+        .form-box {
+          flex: 1;
+        }
+      }
+      .defalt {
+        color: rgb(81, 81, 81);
+        font-size: 14px;
       }
       .active {
-        font-size: 18px;
+        font-size: 24px;
         font-weight: 600;
         color: #000;
-        margin-right: 10px;
-        margin-left: 10px;
       }
+    }
+    .right-info-box {
+      flex: 1;
+      border-radius: 0 10px 10px 0;
+      background-size: cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+    }
+  }
+}
 </style>
--- src/views/RegisterLogin/components/MobileLogin.vue
+++ src/views/RegisterLogin/components/MobileLogin.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>移动端正在开发中...</div>
+  <div>
+    <van-form @submit="onSubmit">
+      <van-cell-group inset>
+        <van-field
+          v-model="formData.username"
+          name="username"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+          v-model="formData.password"
+          type="password"
+          name="password"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+      </van-cell-group>
+      <div style="margin: 16px">
+        <van-button round block type="primary" native-type="submit"> 登录 </van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+const formData = reactive({
+  username: '',
+  password: '',
+})
+
+interface FormValues {
+  username: string;
+  password: string;
+}
+
+const onSubmit = (values: FormValues) => {
+  console.log('submit', values)
+}
+</script>
 
 <style scoped lang="scss"></style>
